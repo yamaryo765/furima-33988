6062b2fd876633fbab4742f12643aba3cf12ede2
yamaryo765 <77099054+yamaryo765@users.noreply.github.com>
2021-02-09 20:43:25 +0900

    バリデーションの正規表現記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
     with_options presence: true do
       validates :nickname
-      validates :family_name,             format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
+      validates :family_name,             format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/ } # ユーザー本名全角の正規表現
       validates :family_name_kana,        format: {with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
-      validates :first_name,              format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
+      validates :first_name,              format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/ } # ユーザー本名全角の正規表現
       validates :first_name_kana,         format: {with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
       validates :password,                format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} 
       validates :birth_day 
