5f248c9d23bb12d156750b3dc02aa46736a55bdf
yamaryo765 <77099054+yamaryo765@users.noreply.github.com>
2021-02-12 17:07:59 +0900

    半角数字の正規表現、テスト修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,15 +9,17 @@ class Item < ApplicationRecord
     belongs_to :shipping_day
     belongs_to :status
 
+    with_options presence: true,numericality: { other_than: 0 } do 
+      validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } ,format:{with: /\A[0-9]+\z/}
+      validates :category_id 
+      validates :status_id
+      validates :shipping_cost_id 
+      validates :shipping_area_id
+      validates :shipping_day_id
+    end
     with_options presence: true do
-      validates :image
-      validates :name
-      validates :description
-      validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } 
-      validates :category_id, numericality: { other_than: 0 } 
-      validates :status_id, numericality: { other_than: 0 } 
-      validates :shipping_cost_id, numericality: { other_than: 0 } 
-      validates :shipping_area_id, numericality: { other_than: 0 } 
-      validates :shipping_day_id, numericality: { other_than: 0 }
+        validates :image
+        validates :name
+        validates :description
     end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -35,30 +35,65 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it  "商品の値段が299円以下では出品できない" do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "商品の値段が10,000,000以上では出品できない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
       it "カテゴリが決められていなければ出品できない" do
         @item.category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it  "商品の状態が分からなければ出品できない" do
+      it "カテゴリidが0以外でないと、出品できない" do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 0")
+      end
+      it "商品の状態が分からなければ出品できない" do
         @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it "状態idが0以外でないと出品できない" do
+        @item.status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 0")
       end
       it "発送日が決められていなければ出品できない" do
         @item.shipping_day_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping day can't be blank")
       end
+      it "発送日idが0以外でないと出品できない" do
+        @item.shipping_day_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day must be other than 0")
+      end
       it "発送元が決められていなければ出品できない" do
         @item.shipping_area_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping area can't be blank")
       end
+      it "発送元idが0以外でないと出品できない" do
+        @item.shipping_area_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area must be other than 0")
+      end
       it "送料込みか着払いか決められていなければ出品できない" do
         @item.shipping_cost_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
+      end
+      it "発送負担idが0以外でないと出品できない" do
+        @item.shipping_cost_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost must be other than 0")
       end
     end
   end
